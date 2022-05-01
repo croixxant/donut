449dfcd22958eb8069db5bc3c5a61798353d9105
croixxant <croixxxant@gmail.com>
2022-05-02 04:48:58 +0900

    add version flag

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/croixxant/donut/cmd"
+import (
+	"github.com/croixxant/donut/cmd"
+	"github.com/croixxant/donut/internal"
+)
+
+var version string
 
 func main() {
+	if len(version) > 0 {
+		internal.SetVersion(version)
+	}
 	cmd.Execute()
 }
